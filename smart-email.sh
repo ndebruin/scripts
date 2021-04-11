90b890dd90888c15c870f21a669fb9ef3003a2ae
draigon0 <73964478+draigon0@users.noreply.github.com>
2021-04-11 17:34:18 -0400

    Create smart-email.sh

---
+++ smart-email.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+EMAIL="<DESTINATION EMAIL>"
+FILE="full.tmp"
+DATA="data.tmp"
+OUTPUT="email.tmp"
+SERVER="<SERVER NAME>"
+
+smartctl -a /dev/$1 > $FILE
+smartctl -A /dev/$1 > $DATA
+
+tail -n +4 $FILE > $FILE.new
+tail -n +7 $DATA > $DATA.new
+
+rm $FILE
+rm $DATA
+
+mv $FILE.new $FILE
+mv $DATA.new $DATA
+
+grep "Device Model:" $FILE > $OUTPUT
+grep "Serial Number:" $FILE >> $OUTPUT
+CAPACITY=$(grep "User Capacity:" $FILE | grep -oP '\[.*?\]' | sed 's/\[//;s/\]//')
+echo "Drive Capacity:   $CAPACITY" >> $OUTPUT
+grep "Local Time is:" $FILE >> $OUTPUT
+RESULT=$(grep "SMART overall-health self-assessment test result:" $FILE | sed -n -e 's/^.*result: //p')
+printf "Result of test:   $RESULT\n\n" >> $OUTPUT
+cat $DATA >> $OUTPUT
+
+rm $FILE
+rm $DATA
+
+cat $OUTPUT | mail -s "SMART Test Results for /dev/$1 on $SERVER" $EMAIL
+rm $OUTPUT
